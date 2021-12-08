f5e1cf612b1b911468480dfb391fd3e8df8b6386
vmic2002 <89990471+vmic2002@users.noreply.github.com>
2021-12-07 19:15:58 -0500

    Add files via upload

---
+++ project1/Letter.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+
+import acm.graphics.GCanvas;
+import acm.graphics.GLine;
+public class Letter {
+	private GRectID grectID;
+	private LineCluster lineCluster;
+	public static GCanvas canvas;
+
+	//maybe need private int id;
+	Letter(LineCluster lineCluster, GRectID grectID){
+		this.lineCluster = lineCluster;
+		this.grectID = grectID;
+		canvas.add(this.grectID);
+	}
+
+	static void setCanvas(GCanvas canvas1) {
+		canvas = canvas1;
+	}
+
+	public GRectID getGRectID() {
+		return grectID;
+	}
+	public LineCluster getLineCluster() {
+		return lineCluster;
+	}
+	public void addLine(GLine line) {
+		line.setColor(Color.BLACK);//this is how to change color of letters (not highlighting them)
+		lineCluster.addLine(line);
+		canvas.add(line);
+	}
+	public void move(double dx, double dy) {
+		grectID.move(dx, dy);
+		if (lineCluster.getLines().size()!=0) {
+			for (GLine l : lineCluster.getLines())
+				l.move(dx,  dy);
+		}
+	}
+	
+	
+
+}
